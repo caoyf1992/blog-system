5080b36d9dda9942bc3ba894ce9c7b0c1c97bd39
caoyf1992 <31468922+caoyf1992@users.noreply.github.com>
2018-05-31 17:23:45 +0800

    修改数据库

--- s10bbs/s10bbs/settings.py
+++ s10bbs/s10bbs/settings.py
@@ -75,24 +75,14 @@ WSGI_APPLICATION = 's10bbs.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#     }
-# }
-
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'bbs',
-        'HOST': '42.62.55.59',
-        'PORT': 3306,
-        'USER': 'root',
-        'PASSWORD': 'cao.cao123'
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
+
+
 
 # Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
